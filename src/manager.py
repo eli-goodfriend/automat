e0662537622a38a5dac0268536b1649b21098c84
Eli Goodfriend <eli.goodfriend@gmail.com>
2017-12-31 16:09:09 -0500

    input is different from setting name to prep for RFID

--- src/manager.py
+++ src/manager.py
@@ -4,6 +4,7 @@ module to manage settings:
     - swaps settings when input changes
 """
 import importlib
+from pyhocon import ConfigFactory
 
 from src.settings import DoNothing
 
@@ -11,22 +12,36 @@ class Manager(object):
     """
     converts a setting command to a room environment
     """
-    def __init__(self, setting_module='src.settings'):
+    def __init__(self, setting_module='src.settings', config='config'):
         self.settings_mod = importlib.import_module(setting_module)
-        
+
         self.current_setting = DoNothing()
         self.current_setting.create()
 
-    def check_setting(self, setting):
+        conf = ConfigFactory.parse_file(config)
+        self.input_to_setting = conf.get('input_to_setting').as_plain_ordered_dict()
+
+    def check_setting(self, setting_input):
         """
         check if setting has changed
         """
+        setting = self.convert_input(setting_input)
         try:
             setting_class = getattr(self.settings_mod, setting)
             if not isinstance(self.current_setting, setting_class):
                 self.change_setting(setting_class)
         except AttributeError:
             print "{} is not a known setting".format(setting)
+
+    def convert_input(self, setting_input):
+        """
+        use config dict to convert between raw input and specified class
+        """
+        try:
+            setting = self.input_to_setting[setting_input]
+        except KeyError:
+            setting = 'DoNothing'
+        return setting
 
     def change_setting(self, new_setting):
         """
